758d4bb8de1149d452509e502821a8905c44f6cf
dayong <dayong@conflux-chain.org>
2022-01-24 18:10:03 +0800

    support get/set provider in client to convinent inject middleware to provider

--- provider_wrapper/provider_middleware.go
+++ provider_wrapper/provider_middleware.go
@@ -13,6 +13,11 @@ type MiddlewarableProvider struct {
 	callContextMiddles      []CallContextMiddleware
 	batchCallMiddles        []BatchCallMiddleware
 	batchCallContextMiddles []BatchCallContextMiddleware
+
+	callNestedWare             CallFunc
+	callContextNestedWare      CallContextFunc
+	batchcallNestedWare        BatchCallFunc
+	batchcallContextNestedWare BatchCallContextFunc
 	// subscribeMiddlewares    []SubscribeMiddleware
 }
 
@@ -34,57 +39,54 @@ type BatchCallContextMiddleware func(BatchCallContextFunc) BatchCallContextFunc
 
 // type SubscribeMiddleware func(SubscribeFunc) SubscribeFunc
 
+// callMiddles: A -> B -> C, execute order is A -> B -> C
 func (mp *MiddlewarableProvider) HookCall(cm CallMiddleware) {
 	mp.callMiddles = append(mp.callMiddles, cm)
+	mp.callNestedWare = mp.Inner.Call
+	for i := len(mp.callMiddles) - 1; i >= 0; i-- {
+		mp.callNestedWare = mp.callMiddles[i](mp.callNestedWare)
+	}
 }
 
 func (mp *MiddlewarableProvider) HookCallContext(cm CallContextMiddleware) {
 	mp.callContextMiddles = append(mp.callContextMiddles, cm)
+	mp.callContextNestedWare = mp.Inner.CallContext
+	for i := len(mp.callContextMiddles) - 1; i >= 0; i-- {
+		mp.callContextNestedWare = mp.callContextMiddles[i](mp.callContextNestedWare)
+	}
 }
 
 func (mp *MiddlewarableProvider) HookBatchCall(cm BatchCallMiddleware) {
 	mp.batchCallMiddles = append(mp.batchCallMiddles, cm)
+	mp.batchcallNestedWare = mp.Inner.BatchCall
+	for i := len(mp.batchCallMiddles) - 1; i >= 0; i-- {
+		mp.batchcallNestedWare = mp.batchCallMiddles[i](mp.batchcallNestedWare)
+	}
+
 }
 
 func (mp *MiddlewarableProvider) HookBatchCallContext(cm BatchCallContextMiddleware) {
 	mp.batchCallContextMiddles = append(mp.batchCallContextMiddles, cm)
+	mp.batchcallContextNestedWare = mp.Inner.BatchCallContext
+	for i := len(mp.batchCallContextMiddles) - 1; i >= 0; i-- {
+		mp.batchcallContextNestedWare = mp.batchCallContextMiddles[i](mp.batchcallContextNestedWare)
+	}
 }
 
 func (mp *MiddlewarableProvider) Call(resultPtr interface{}, method string, args ...interface{}) error {
-	// callMiddles: A -> B -> C, execute order is A -> B -> C
-	var nestedWare CallFunc = mp.Inner.Call
-	for i := len(mp.callMiddles) - 1; i >= 0; i-- {
-		nestedWare = mp.callMiddles[i](nestedWare)
-	}
-
-	return nestedWare(resultPtr, method, args...)
+	return mp.callNestedWare(resultPtr, method, args...)
 }
 
 func (mp *MiddlewarableProvider) CallContext(ctx context.Context, resultPtr interface{}, method string, args ...interface{}) error {
-	var nestedWare CallContextFunc = mp.Inner.CallContext
-	for i := len(mp.callContextMiddles) - 1; i >= 0; i-- {
-		nestedWare = mp.callContextMiddles[i](nestedWare)
-	}
-
-	return nestedWare(ctx, resultPtr, method, args...)
+	return mp.callContextNestedWare(ctx, resultPtr, method, args...)
 }
 
 func (mp *MiddlewarableProvider) BatchCall(b []rpc.BatchElem) error {
-	var nestedWare BatchCallFunc = mp.Inner.BatchCall
-	for i := len(mp.batchCallMiddles) - 1; i >= 0; i-- {
-		nestedWare = mp.batchCallMiddles[i](nestedWare)
-	}
-
-	return nestedWare(b)
+	return mp.batchcallNestedWare(b)
 }
 
 func (mp *MiddlewarableProvider) BatchCallContext(ctx context.Context, b []rpc.BatchElem) error {
-	var nestedWare BatchCallContextFunc = mp.Inner.BatchCallContext
-	for i := len(mp.batchCallContextMiddles) - 1; i >= 0; i-- {
-		nestedWare = mp.batchCallContextMiddles[i](nestedWare)
-	}
-
-	return nestedWare(ctx, b)
+	return mp.batchcallContextNestedWare(ctx, b)
 }
 
 func (mp *MiddlewarableProvider) Subscribe(ctx context.Context, namespace string, channel interface{}, args ...interface{}) (*rpc.ClientSubscription, error) {
