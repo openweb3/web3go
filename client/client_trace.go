5071492292fc5b86f321b7cfffb94b333a7e9ea1
dayong <wangdayong228@163.com>
2022-05-26 10:48:52 +0800

    composite MiddlewareProvider to Client for convinenet to use HookCallContext/HookBatchCallContext

--- client/client_trace.go
+++ client/client_trace.go
@@ -7,16 +7,17 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
 	"github.com/openweb3/go-rpc-provider/interfaces"
+	providers "github.com/openweb3/go-rpc-provider/provider_wrapper"
 	"github.com/openweb3/web3go/types"
 )
 
 type RpcTraceClient struct {
-	core interfaces.Provider
+	*providers.MiddlewarableProvider
 }
 
 func NewRpcTraceClient(provider interfaces.Provider) *RpcTraceClient {
 	return &RpcTraceClient{
-		core: provider,
+		MiddlewarableProvider: providers.NewMiddlewarableProvider(provider),
 	}
 }
 
@@ -25,31 +26,31 @@ func NewRpcTraceClient(provider interfaces.Provider) *RpcTraceClient {
 
 /// Returns traces matching given filter.
 func (c *RpcTraceClient) Filter(traceFilter types.TraceFilter) (val []types.LocalizedTrace, err error) {
-	err = c.core.CallContext(context.Background(), &val, "trace_filter", traceFilter)
+	err = c.CallContext(context.Background(), &val, "trace_filter", traceFilter)
 	return
 }
 
 /// Returns transaction trace at given index.
 func (c *RpcTraceClient) Trace(transactionHash common.Hash, indexes []uint) (val *types.LocalizedTrace, err error) {
-	err = c.core.CallContext(context.Background(), &val, "trace_get", transactionHash, indexes)
+	err = c.CallContext(context.Background(), &val, "trace_get", transactionHash, indexes)
 	return
 }
 
 /// Returns all traces of given transaction.
 func (c *RpcTraceClient) Transactions(transactionHash common.Hash) (val []types.LocalizedTrace, err error) {
-	err = c.core.CallContext(context.Background(), &val, "trace_transaction", transactionHash)
+	err = c.CallContext(context.Background(), &val, "trace_transaction", transactionHash)
 	return
 }
 
 /// Returns all traces produced at given block.
 func (c *RpcTraceClient) Blocks(blockNumber types.BlockNumberOrHash) (val []types.LocalizedTrace, err error) {
-	err = c.core.CallContext(context.Background(), &val, "trace_block", blockNumber)
+	err = c.CallContext(context.Background(), &val, "trace_block", blockNumber)
 	return
 }
 
 /// Executes the given call and returns a number of possible traces for it.
 func (c *RpcTraceClient) Call(request types.CallRequest, options types.TraceOptions, blockNumber *types.BlockNumberOrHash) (val types.TraceResults, err error) {
-	err = c.core.CallContext(context.Background(), &val, "trace_call", request, options, blockNumber)
+	err = c.CallContext(context.Background(), &val, "trace_call", request, options, blockNumber)
 	return
 }
 
@@ -57,7 +58,7 @@ func (c *RpcTraceClient) Call(request types.CallRequest, options types.TraceOpti
 // func(c *RpcTraceClient) CallMany(_ types.Vec<(CallRequest, TraceOptions)>, _ *types.BlockNumber)(val []types.TraceResults, err error) {
 //     TraceOptions)>
 
-//     err = c.core.CallContext(context.Background(),&val, "trace_callMany", _, TraceOptions)>, _)
+//     err = c.CallContext(context.Background(),&val, "trace_callMany", _, TraceOptions)>, _)
 
 //     return
 // }
@@ -65,18 +66,18 @@ func (c *RpcTraceClient) Call(request types.CallRequest, options types.TraceOpti
 /// Executes the given raw transaction and returns a number of possible traces for it.
 func (c *RpcTraceClient) RawTransaction(rawTransaction []byte, options types.TraceOptions, blockNumber *types.BlockNumberOrHash) (val types.TraceResults, err error) {
 	_rawTransaction := (hexutil.Bytes)(rawTransaction)
-	err = c.core.CallContext(context.Background(), &val, "trace_rawTransaction", _rawTransaction, options, blockNumber)
+	err = c.CallContext(context.Background(), &val, "trace_rawTransaction", _rawTransaction, options, blockNumber)
 	return
 }
 
 /// Executes the transaction with the given hash and returns a number of possible traces for it.
 func (c *RpcTraceClient) ReplayTransaction(transactionHash common.Hash, options types.TraceOptions) (val types.TraceResults, err error) {
-	err = c.core.CallContext(context.Background(), &val, "trace_replayTransaction", transactionHash, options)
+	err = c.CallContext(context.Background(), &val, "trace_replayTransaction", transactionHash, options)
 	return
 }
 
 /// Executes all the transactions at the given block and returns a number of possible traces for each transaction.
 func (c *RpcTraceClient) ReplayBlockTransactions(blockNumber types.BlockNumberOrHash, options types.TraceOptions) (val []types.TraceResultsWithTransactionHash, err error) {
-	err = c.core.CallContext(context.Background(), &val, "trace_replayBlockTransactions", blockNumber, options)
+	err = c.CallContext(context.Background(), &val, "trace_replayBlockTransactions", blockNumber, options)
 	return
 }
