9012c4ba47d81b79c6b2d7292925954522577d69
dayong <wangdayong228@163.com>
2022-06-15 18:47:17 +0800

    optimize

--- client.go
+++ client.go
@@ -2,13 +2,14 @@ package web3go
 
 import (
 	"github.com/openweb3/go-rpc-provider/interfaces"
-	providers "github.com/openweb3/go-rpc-provider/provider_wrapper"
+	pproviders "github.com/openweb3/go-rpc-provider/provider_wrapper"
 	client "github.com/openweb3/web3go/client"
+	"github.com/openweb3/web3go/providers"
 )
 
 // Client defines typed wrappers for the Ethereum RPC API.
 type Client struct {
-	*providers.MiddlewarableProvider
+	*pproviders.MiddlewarableProvider
 	option *ClinetOption
 	Eth    *client.RpcEthClient
 	Trace  *client.RpcTraceClient
@@ -17,14 +18,18 @@ type Client struct {
 
 func NewClient(rawurl string) (*Client, error) {
 	return NewClientWithOption(rawurl, &ClinetOption{
-		&providers.Option{}, nil,
+		&pproviders.Option{}, nil,
 	})
 }
 
 func NewClientWithOption(rawurl string, option *ClinetOption) (*Client, error) {
-	p, err := providers.NewProviderWithOption(rawurl, *option.Option)
+	p, err := pproviders.NewProviderWithOption(rawurl, *option.Option)
 	if err != nil {
 		return nil, err
+	}
+
+	if option.SignerManager != nil {
+		p = providers.NewSignableProvider(p, option.SignerManager)
 	}
 
 	ec := NewClientWithProvider(p)
@@ -40,12 +45,16 @@ func NewClientWithProvider(p interfaces.Provider) *Client {
 }
 
 func (c *Client) SetProvider(p interfaces.Provider) {
-	c.MiddlewarableProvider = providers.NewMiddlewarableProvider(p)
+	if _, ok := p.(*pproviders.MiddlewarableProvider); !ok {
+		p = pproviders.NewMiddlewarableProvider(p)
+	}
+
+	c.MiddlewarableProvider = p.(*pproviders.MiddlewarableProvider)
 	c.Eth = client.NewRpcEthClient(p)
 	c.Trace = client.NewRpcTraceClient(p)
 	c.Parity = client.NewRpcParityClient(p)
 }
 
-func (c *Client) Provider() *providers.MiddlewarableProvider {
+func (c *Client) Provider() *pproviders.MiddlewarableProvider {
 	return c.MiddlewarableProvider
 }
